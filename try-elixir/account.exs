0cbf37d31cc4a41c9c942dab72a1ce42712ac2c7
Angel Rojo Marijuan <arojomarijuan@magento.com>
2017-08-20 00:02:36 +0200

    add account.exs - namespaces and named functions

---
+++ try-elixir/account.exs
@@ -0,0 +1,9 @@
+defmodule Account do
+  def investment_return(initial,interest) do
+    initial + (initial * interest)
+  end
+end
+
+amount = Account.investment_return(1000, 0.0001)
+IO.puts "Investment return: $#{amount}"
+
