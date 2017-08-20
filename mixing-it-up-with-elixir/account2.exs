dd9c33eb14b11725d37c34076931e2910c22016b
Angel Rojo Marijuan <arojomarijuan@magento.com>
2017-08-20 11:44:18 +0200

    adding account2.exs - tuples in function signature

---
+++ mixing-it-up-with-elixir/account2.exs
@@ -0,0 +1,13 @@
+defmodule Account do
+  def list_transactions({:ok, content}) do
+    count = length(content)
+    IO.puts "Transactions files: #{count}"
+  end
+  
+  def list_transactions({:error, _}) do
+    IO.puts "Error listing files"
+  end
+end
+
+File.ls("/home/transactions/") |> Account.list_transactions()
+
