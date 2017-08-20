df52576761eca52c0f0c10183af7920fea6c7a7c
Angel Rojo Marijuan <arojomarijuan@magento.com>
2017-08-20 11:22:51 +0200

    modify script.exs - multi-arity anonymous functions

--- mixing-it-up-with-elixir/script.exs
+++ mixing-it-up-with-elixir/script.exs
@@ -4,3 +4,11 @@ print_name.("Angel")
 print_double = fn(number) -> number * 2 end
 IO.puts print_double.(30)
 
+perform_operation = fn
+  (values, :addition) -> Enum.reduce(values, &(&1 + &2))
+  (values, :multiplication) -> Enum.reduce(values, &(&1 * &2))
+end
+
+IO.puts perform_operation.([1, 2, 3, 4], :addition)
+IO.puts perform_operation.([1, 2, 3, 4], :multiplication)
+
