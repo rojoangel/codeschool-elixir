177f6da02e07e174cf5dad0b2e5d8edf03ec2f04
Angel Rojo Marijuan <arojomarijuan@magento.com>
2017-08-20 12:02:07 +0200

    modify script.exs - nested map pattern matching

--- mixing-it-up-with-elixir/script.exs
+++ mixing-it-up-with-elixir/script.exs
@@ -25,3 +25,10 @@ person = %{ "name" => "Sam", "age" => 31 }
 %{ "age" => age }  = person
 IO.puts "Age is #{age}"
 
+person = %{ "name" => "Sam", "age" => 31,
+  "bank_info" => %{ "routing" => "001002", "account" => "123123" }
+}
+
+%{ "bank_info" => %{ "account" => account } }= person
+IO.puts "Bank Account is #{account}"
+
